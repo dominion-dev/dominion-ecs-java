ccb8480f06fe95d3c9a4b64dc3f8c1b5f320ae65
enricostara <enrico.stara@gmail.com>
2021-12-04 13:25:34 +0100

    Add the maven flatten plugin to deploy dominion-ecs modules by resolving the revision property before publish

--- dominion-ecs-engine/src/test/java/module-info.java
+++ dominion-ecs-engine/src/test/java/module-info.java
@@ -1,12 +1,5 @@
-module dev.dominion.ecs.test.engine {
+open module dev.dominion.ecs.test.engine {
     requires dev.dominion.ecs.engine;
     requires org.junit.jupiter.api;
     requires org.junit.jupiter.engine;
-
-    opens dev.dominion.ecs.test.engine
-            to org.junit.platform.commons;
-    opens dev.dominion.ecs.test.engine.collections
-            to org.junit.platform.commons;
-    opens dev.dominion.ecs.test.engine.system
-            to org.junit.platform.commons;
 }
