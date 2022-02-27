1a6dbc669675d7f9a7aea589b4d19d25ec8fca8b
enricostara <enrico.stara@gmail.com>
2022-02-27 11:47:10 +0100

    Prune unused Results records

--- dominion-ecs-api/src/main/java/dev/dominion/ecs/api/Results.java
+++ dominion-ecs-api/src/main/java/dev/dominion/ecs/api/Results.java
@@ -33,12 +33,4 @@ public interface Results<T> {
 
     record Comp6<T1, T2, T3, T4, T5, T6>(T1 comp1, T2 comp2, T3 comp3, T4 comp4, T5 comp5, T6 comp6, Entity entity) {
     }
-
-    record Comp7<T1, T2, T3, T4, T5, T6, T7>(T1 comp1, T2 comp2, T3 comp3, T4 comp4, T5 comp5, T6 comp6, T7 comp7,
-                                             Entity entity) {
-    }
-
-    record Comp8<T1, T2, T3, T4, T5, T6, T7, T8>(T1 comp1, T2 comp2, T3 comp3, T4 comp4, T5 comp5, T6 comp6, T7 comp7,
-                                                 T8 comp8, Entity entity) {
-    }
 }
