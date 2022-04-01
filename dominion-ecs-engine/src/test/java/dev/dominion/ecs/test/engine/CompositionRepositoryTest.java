38cbca860101c193768f056d179b548c8989849a
enricostara <enrico.stara@gmail.com>
2022-04-01 20:18:51 +0200

    Add Results.excludeWith unit tests

--- dominion-ecs-engine/src/test/java/dev/dominion/ecs/test/engine/CompositionRepositoryTest.java
+++ dominion-ecs-engine/src/test/java/dev/dominion/ecs/test/engine/CompositionRepositoryTest.java
@@ -3,13 +3,13 @@ package dev.dominion.ecs.test.engine;
 import dev.dominion.ecs.engine.Composition;
 import dev.dominion.ecs.engine.CompositionRepository;
 import dev.dominion.ecs.engine.collections.ChunkedPool.IdSchema;
+import dev.dominion.ecs.engine.system.ClassIndex;
 import dev.dominion.ecs.engine.system.IndexKey;
 import dev.dominion.ecs.engine.system.LoggingSystem;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 import java.util.Map;
 
 class CompositionRepositoryTest {
@@ -93,48 +93,74 @@ class CompositionRepositoryTest {
     @Test
     void findWith() {
         try (CompositionRepository compositionRepository = new CompositionRepository(LoggingSystem.Context.TEST)) {
+            ClassIndex classIndex = compositionRepository.getClassIndex();
             Composition compositionC1 = compositionRepository.getOrCreate(new Object[]{new C1(0)});
             Composition compositionC1C2 = compositionRepository.getOrCreate(new Object[]{new C1(0), new C2(0)});
             Composition compositionC2C3 = compositionRepository.getOrCreate(new Object[]{new C2(0), new C3(0)});
             Composition compositionC2 = compositionRepository.getOrCreate(new Object[]{new C2(0)});
 
-            Map<IndexKey, CompositionRepository.Node> nodes = compositionRepository.findWith(C1.class);
-            Assertions.assertNotNull(nodes);
-            List<CompositionRepository.Node> nodeList = nodes.values().stream().toList();
-            Assertions.assertEquals(2, nodeList.size());
-            Assertions.assertEquals(compositionC1, nodeList.get(0).getComposition());
-            Assertions.assertEquals(compositionC1C2, nodeList.get(1).getComposition());
+            IndexKey compositionC1Key = new IndexKey(classIndex.getIndex(C1.class));
+            IndexKey compositionC1C2Key = classIndex.getIndexKey(new Object[]{new C1(0), new C2(0)});
+            IndexKey compositionC2C3Key = classIndex.getIndexKey(new Object[]{new C2(0), new C3(0)});
+            IndexKey compositionC2Key = new IndexKey(classIndex.getIndex(C2.class));
 
-            nodes = compositionRepository.findWith(C2.class);
-            Assertions.assertNotNull(nodes);
-            nodeList = nodes.values().stream().toList();
-            Assertions.assertEquals(3, nodeList.size());
-            Assertions.assertEquals(compositionC1C2, nodeList.get(0).getComposition());
-            Assertions.assertEquals(compositionC2C3, nodeList.get(1).getComposition());
-            Assertions.assertEquals(compositionC2, nodeList.get(2).getComposition());
+            Map<IndexKey, CompositionRepository.Node> nodeMap = compositionRepository.findWith(C1.class);
+            Assertions.assertNotNull(nodeMap);
+            Assertions.assertEquals(2, nodeMap.size());
+            Assertions.assertEquals(compositionC1, nodeMap.get(compositionC1Key).getComposition());
+            Assertions.assertEquals(compositionC1C2, nodeMap.get(compositionC1C2Key).getComposition());
 
-            nodes = compositionRepository.findWith(C3.class);
-            Assertions.assertNotNull(nodes);
-            nodeList = nodes.values().stream().toList();
-            Assertions.assertEquals(1, nodeList.size());
-            Assertions.assertEquals(compositionC2C3, nodeList.get(0).getComposition());
+            nodeMap = compositionRepository.findWith(C2.class);
+            Assertions.assertNotNull(nodeMap);
+            Assertions.assertEquals(3, nodeMap.size());
+            Assertions.assertEquals(compositionC1C2, nodeMap.get(compositionC1C2Key).getComposition());
+            Assertions.assertEquals(compositionC2C3, nodeMap.get(compositionC2C3Key).getComposition());
+            Assertions.assertEquals(compositionC2, nodeMap.get(compositionC2Key).getComposition());
 
-            nodes = compositionRepository.findWith(C2.class, C1.class);
-            Assertions.assertNotNull(nodes);
-            nodeList = nodes.values().stream().toList();
-            Assertions.assertEquals(1, nodeList.size());
-            Assertions.assertEquals(compositionC1C2, nodeList.get(0).getComposition());
+            nodeMap = compositionRepository.findWith(C3.class);
+            Assertions.assertNotNull(nodeMap);
+            Assertions.assertEquals(1, nodeMap.size());
+            Assertions.assertEquals(compositionC2C3, nodeMap.get(compositionC2C3Key).getComposition());
 
-            nodes = compositionRepository.findWith(C3.class, C2.class);
-            Assertions.assertNotNull(nodes);
-            nodeList = nodes.values().stream().toList();
-            Assertions.assertEquals(1, nodeList.size());
-            Assertions.assertEquals(compositionC2C3, nodeList.get(0).getComposition());
+            nodeMap = compositionRepository.findWith(C2.class, C1.class);
+            Assertions.assertNotNull(nodeMap);
+            Assertions.assertEquals(1, nodeMap.size());
+            Assertions.assertEquals(compositionC1C2, nodeMap.get(compositionC1C2Key).getComposition());
 
-            nodes = compositionRepository.findWith(C3.class, C1.class);
-            Assertions.assertNotNull(nodes);
-            nodeList = nodes.values().stream().toList();
-            Assertions.assertEquals(0, nodeList.size());
+            nodeMap = compositionRepository.findWith(C3.class, C2.class);
+            Assertions.assertNotNull(nodeMap);
+            Assertions.assertEquals(1, nodeMap.size());
+            Assertions.assertEquals(compositionC2C3, nodeMap.get(compositionC2C3Key).getComposition());
+
+            nodeMap = compositionRepository.findWith(C3.class, C1.class);
+            Assertions.assertNotNull(nodeMap);
+            Assertions.assertEquals(0, nodeMap.size());
+        }
+    }
+
+    @Test
+    void removeFrom() {
+        try (CompositionRepository compositionRepository = new CompositionRepository(LoggingSystem.Context.TEST)) {
+            ClassIndex classIndex = compositionRepository.getClassIndex();
+            Composition compositionC1 = compositionRepository.getOrCreate(new Object[]{new C1(0)});
+            Composition compositionC1C2 = compositionRepository.getOrCreate(new Object[]{new C1(0), new C2(0)});
+            Composition compositionC1C2C3 = compositionRepository.getOrCreate(new Object[]{new C1(0), new C2(0), new C3(0)});
+
+            IndexKey compositionC1Key = new IndexKey(classIndex.getIndex(C1.class));
+            IndexKey compositionC1C2Key = classIndex.getIndexKey(new Object[]{new C1(0), new C2(0)});
+            IndexKey compositionC1C2C3Key = classIndex.getIndexKey(new Object[]{new C1(0), new C2(0), new C3(0)});
+
+            Map<IndexKey, CompositionRepository.Node> nodeMap = compositionRepository.findWith(C1.class);
+            Assertions.assertNotNull(nodeMap);
+            Assertions.assertEquals(3, nodeMap.size());
+            Assertions.assertEquals(compositionC1, nodeMap.get(compositionC1Key).getComposition());
+            Assertions.assertEquals(compositionC1C2, nodeMap.get(compositionC1C2Key).getComposition());
+            Assertions.assertEquals(compositionC1C2C3, nodeMap.get(compositionC1C2C3Key).getComposition());
+
+            compositionRepository.removeFrom(nodeMap, C2.class);
+            Assertions.assertNotNull(nodeMap);
+            Assertions.assertEquals(1, nodeMap.size());
+            Assertions.assertEquals(compositionC1, nodeMap.get(compositionC1Key).getComposition());
         }
     }
 
