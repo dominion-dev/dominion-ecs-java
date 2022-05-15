36f05c3ccb508fb4bd92b1cd154963e226356192
enricostara <enrico.stara@gmail.com>
2022-05-15 21:25:04 +0200

    Add GPG, license-check, and nexus plugins to parent pom

--- dominion-ecs-engine/src/main/java/module-info.java
+++ dominion-ecs-engine/src/main/java/module-info.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2021 Enrico Stara
+ * This code is licensed under the MIT license. See the LICENSE file in the project root for license terms.
+ */
+
 import dev.dominion.ecs.api.Dominion;
 import dev.dominion.ecs.engine.EntityRepository;
 
